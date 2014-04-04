5f68270b409e1d2fc87f21fb6672c11268465044
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-04-04 22:17:40 +0200

    Factorize moving marker and story narration

--- js/event_manager.js
+++ js/event_manager.js
@@ -79,91 +79,28 @@ function showHideInterface(){
 //Set hero position to clicked marker
 /*for (var i = 0; i < markerMusees.length; i++) {
     markerMusees[i].on('click', function(event) {*/
-function moveMarkerMusees(marker){
-        var position = markerMusees[marker].getLatLng();
+function moveMarker(marker, condition){
+        switch(condition){
+            case 'bar':
+                var position = markerPub[marker].getLatLng();
+            break;
+            case 'hopital':
+                var position = markerHopitaux[marker].getLatLng();
+            break;
+            case 'musee':
+                var position = markerMusees[marker].getLatLng();
+            break;
+            case 'commissariat':
+                var position = markerCommissariats[marker].getLatLng();
+            break;
+
+        }
+        
         hero.setLatLng(position);
         circle.setLatLng(position);
         polyline.addLatLng(position);
         map.closePopup();
         displayMarker();
         map.setView(position, 15);    
-        storyMusees();
-}
-
-    /*});*/
-/*    markerMusees[i].on('contextmenu', function(event){
-        this.bindPopup(popupMusees[i]);
-        console.log(popupMusees[i]);
-        this.openPopup();
-        this.unbindPopup();
-    });*/
-/*}*/
-/*for (var i = 0; i < markerCommissariats.length; i++) {
-    markerCommissariats[i].on('click', function(event) {*/
-function moveMarkerCommissariats(marker){
-        var position = markerCommissariats[marker].getLatLng();
-        hero.setLatLng(position);
-        circle.setLatLng(position);
-        polyline.addLatLng(position);
-        map.closePopup();
-        displayMarker();
-        map.setView(position, 15);
-        storyCommissariats();
-}
-
-    /*});*/
-/*    markerCommissariats[i].on('contextmenu', function(event){
-        this.bindPopup(popupCommissariats[i]);
-        console.log(popupCommissariats[i]);
-        this.openPopup();
-        this.unbindPopup();
-    });*/
-/*}*/
-function moveMarkerHopitaux(marker){
-    //for (var i = 0; i < markerHopitaux.length; i++) {
-    //markerHopitaux[i].on('click', function(event) {
-        var position = markerHopitaux[marker].getLatLng();
-        hero.setLatLng(position);
-        circle.setLatLng(position);
-        polyline.addLatLng(position);
-        map.closePopup();
-        displayMarker();
-        map.setView(position, 15);
-        storyHopitaux();
-   //});
-//});
-
-/*    markerHopitaux[i].on('contextmenu', function(event){
-        this.bindPopup(popupHopitaux[i]);
-        console.log(popupHopitaux);
-        this.openPopup();
-        this.unbindPopup();
-    });*/
-}
-
-function moveMarkerPub(marker){
-   // for (var i = 0; i < markerPub.length; i++) {
-    /*console.log("i2: " + i);*/
-/*        markerPub[i].on('contextmenu', function(event){
-        console.log(popupPub[240]);
-        console.log('i: ' + i);
-        this.bindPopup(popupPub[i]);
-        
-        this.openPopup();
-        this.unbindPopup();
-    });
-
-*/    //markerPub[i].on('click', function(event) {
-        var position = markerPub[marker].getLatLng();
-        hero.setLatLng(position);
-        circle.setLatLng(position);
-        polyline.addLatLng(position);
-        map.closePopup();
-        displayMarker();
-        map.setView(position, 15);
-        storyPub();
-    //});
-
-//}
-
+        storyNext(condition);
 }
--- js/story_manager.js
+++ js/story_manager.js
@@ -32,120 +32,22 @@ function story_manager(){
         Game.condition[conseq][0] = story[window.ref].conseq_ref[conseq][2];
         Game.condition[conseq][1] = story[window.ref].conseq_ref[conseq][0];
 
-        /** Bind choice to marker type **/
-        /*if(story[window.ref].conseq_ref[conseq][2] === "musees"){  // !! Check if correct !!
-            for (var i = 0; i < musees.features.length; i++) {
-                    markerMusees[i].on('click', function(){
-                    window.ref = story[window.ref].conseq_ref[conseq][0];
-                    //story_manager();
-                });
-            } 
-        }*/
-        /*else if(story[window.ref].conseq_ref[conseq][2] === "commissariat"){*/
-           /* for (var i = 0; i < commissariats.features.length; i++) {
-                    markerCommissariats[i].on('click', function(){
-
-                    console.log('location: ' + story[window.ref].conseq_ref[conseq][2]);
-                    console.log('conseq ref: ' + story[window.ref].conseq_ref[conseq][0]);
-                    console.log('conseq: ' + conseq);
-                    window.ref = story[window.ref].conseq_ref[conseq][0];
-                    //story_manager();
-                });
-            }*/
-
-           /* commissariatsLayer.on('click', function (){
-                story_manager();
-                console.log(story[window.ref].conseq_ref[conseq][0]);
-                window.ref = story[window.ref].conseq_ref[conseq][0];
-            }); 
-        }*//*
-        else if(story[window.ref].conseq_ref[conseq][2] === "hopitaux"){
-            for (var i = 0; i < hopitaux.features.length; i++) {
-                    markerHopitaux[i].on('click', function(){
-                    window.ref = story[window.ref].conseq_ref[conseq][0];
-                    //story_manager();
-                }); 
-            }
-        }
-        else if(story[window.ref].conseq_ref[conseq][2] === "bar"){*/
-            /*for (var i = 0; i < pub.elements.length; i++) { 
-                if(pub.elements[i].lat && pub.elements[i].lon){ //Protect if data corrumpted
-
-                    markerPub[i].on('click', function(){
-                    console.log('location: ' + story[window.ref].conseq_ref[conseq][2]);
-                    console.log(' iflocation: ' + (story[window.ref].conseq_ref[conseq][2] === "bar"));
-                    console.log('conseq ref: ' + story[window.ref].conseq_ref[conseq][0]);
-                    console.log('conseq: ' + conseq);
-                    window.ref = story[window.ref].conseq_ref[conseq][0];
-
-                    console.log('Pub click');
-                    //story_manager();
-                    
-
-                    }); 
-                }   
-            }*/
-
-            /*pubsLayer.on('click', function (){
-                window.ref = story[window.ref].conseq_ref[conseq][0];
-                story_manager();
-            });
-        }
-        else{
-            
-        }*/
-
     }
 }
 
 
 //pubsLayer.on('click', function (){
     //console.log(Game.condition);
-function storyPub(){
+    //
+    //
+function storyNext(inputCondition){
     for ( var i in Game.condition){
         console.log(Game.condition[i][1]);
-        if(Game.condition[i][0] === "bar"){
+        if(Game.condition[i][0] === inputCondition){
             console.log(Game.condition[i][1]);
                 window.ref = Game.condition[i][1];
                 story_manager();
         }
     //}
-}    
-}    
-//});
-
-//commissariatsLayer.on('click', function (){
-function storyCommissariats(){
-    for ( var i in Game.condition){
-        if(Game.condition[i][0] === "commissariat"){
-                window.ref = Game.condition[i][1];
-                story_manager();
-        }
-    }    
+    }
 }
-
-//});
-
-//museesLayer.on('click', function (){
-function storyMusees(){
-    for ( var i in Game.condition){
-        if(Game.condition[i][0] === "musee"){
-                window.ref = Game.condition[i][1];
-                story_manager();
-        }
-    }    
-}
-
-//});
-
-//hopitauxLayer.on('click', function (){
-function storyHopitaux(){
-    for ( var i in Game.condition){
-        if(Game.condition[i][0] === "hopital"){
-                window.ref = Game.condition[i][1];
-                story_manager();
-        }
-    }   
-}
-
-//});
